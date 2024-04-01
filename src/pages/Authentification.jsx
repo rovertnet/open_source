ee051d87e12703754af0b8996718dda412b883a4
Robert Matundu <matundukabamba@gmail.com>
2024-04-01 16:45:56 +0200

    nouveau commit

--- src/pages/Authentification.jsx
+++ src/pages/Authentification.jsx
@@ -23,6 +23,7 @@ const Authentification = () => {
               autoComplete="off"
               method="POST"
             >
+
               <div>
                 <label
                   htmlFor="email"
@@ -36,25 +37,6 @@ const Authentification = () => {
                     name="email"
                     type="email"
                     autoComplete="email"
-                    required
-                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-
-              <div>
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Email address
-                </label>
-                <div className="mt-2">
-                  <input
-                    id="text"
-                    name="text"
-                    type="text"
-                    autoComplete="text"
                     required
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
