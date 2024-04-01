72c3f76083f1478dc66ddf4d031f079fcb995a66
Robert Matundu <matundukabamba@gmail.com>
2024-04-01 23:21:48 +0200

    nouveau commit

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -104,7 +104,7 @@ const Navbar = () => {
             </button>
 
              <div className="-mr-2 flex md:hidden">
-                    {/* Mobile menu button */}
+                {/* Mobile menu button */}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-0.5" />
                   <span className="sr-only">Open main menu</span>
