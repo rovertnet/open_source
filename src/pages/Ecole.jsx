c9967c4397763441ef8057eeafb6483248b34338
Robert Matundu <matundukabamba@gmail.com>
2024-04-01 17:51:14 +0200

    nouveau commit

--- src/pages/Ecole.jsx
+++ src/pages/Ecole.jsx
@@ -1,20 +1,19 @@
 import { Outlet } from "react-router-dom"
 import Navbar from "../components/header/Navbar";
 import Panel from "../components/component_ecole/Panel";
+import PageCompoonent from "../components/PageCompoonent";
 
 
 const Ecole = () => {
   return (
-    <div className=" bg-blanc">
+    <>
       <Navbar />
       <Outlet />
-      <div className=" bg-dark p-3 shadow-md">
-        <h1 className=" text-2xl px-14 text-white font-bold font-pacifico">
-          Scool
-        </h1>
-      </div>
-      <Panel />
-    </div>
+
+      <PageCompoonent title="Scool">
+        <Panel />
+      </PageCompoonent>
+    </>
   );
 }
 
