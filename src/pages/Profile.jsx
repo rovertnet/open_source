fae27898ec49eb00b0f70468468f5a3c69e18d7d
Robert Matundu <matundukabamba@gmail.com>
2024-04-01 18:02:52 +0200

    nouveau commit

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,18 @@
+import { Navbar } from "flowbite-react";
+import { Outlet } from "react-router-dom";
+import PageCompoonent from "../components/PageCompoonent";
 
 const Profile = () => {
   return (
     <>
-      
+      <Navbar />
+      <Outlet />
+
+      <PageCompoonent title={"Profile"} >
+        Profile
+      </PageCompoonent>
     </>
-  )
+  );
 }
 
 export default Profile
