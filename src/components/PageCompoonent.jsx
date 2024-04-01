f9b1d8e06732ac3405d9dde123b0cb1682d53d7e
Robert Matundu <matundukabamba@gmail.com>
2024-04-01 17:44:57 +0200

    nouveau commit

---
+++ src/components/PageCompoonent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const PageCompoonent = ({ title, button = "", children }) => {
+  return (
+    <>
+      <header className="bg-dark p-3 shadow-md">
+        <div className="flex justify-between items-center mx-auto max-w-7xl py-4 px-14">
+          <h1 className=" text-2xl font-bold tracking-tight text-white">
+            {" "}
+            {title}{" "}
+          </h1>
+          {button}
+        </div>
+      </header>
+      <main>
+        <div className="">{children}</div>
+      </main>
+    </>
+  );
+};
+
+export default PageCompoonent;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,17 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/header/Navbar";
+import PageCompoonent from "../components/PageCompoonent";
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Outlet />
-      <div className=" bg-dark p-3 shadow-md">
-        <h1 className=" text-2xl px-14 text-white font-bold font-pacifico">
-          DashBoard
-        </h1>
-      </div>
+      
+      <PageCompoonent>
+        
+      </PageCompoonent>
+
     </>
   );
 };
