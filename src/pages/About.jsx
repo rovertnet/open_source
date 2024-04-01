17dd3dfb2ecafd682ae5b5a9037b3ad9d9f5dae2
Robert Matundu <matundukabamba@gmail.com>
2024-04-02 00:54:05 +0200

    nouveau commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/header/Navbar"
 import PageCompoonent from "../components/PageCompoonent";
+import NewLetter from "../components/NewLetter";
 
 const About = () => {
   return (
@@ -8,8 +9,8 @@ const About = () => {
       <Navbar />
       <Outlet />
 
-      <PageCompoonent title="About" >
-        A propos
+      <PageCompoonent title="About">
+        <NewLetter />
       </PageCompoonent>
     </>
   );
