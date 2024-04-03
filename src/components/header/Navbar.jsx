fc0d082f5a6b05b3c7c0fae27521787f7e066411
Robert Matundu <matundukabamba@gmail.com>
2024-04-03 14:46:12 +0200

    update

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -108,66 +108,67 @@ const Navbar = () => {
                 </Transition>
               </Menu>
             </button>
-            <div>
-              <div className="-mr-2 flex md:hidden">
-                {/* Mobile menu button */}
-                <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-            </div>
           </div>
 
-          <Disclosure.Panel className={" md:hidden"}>
-            <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-              {navigation.map((item) => (
+          <div>
+            <div className="-mr-2 flex md:hidden">
+              {/* Mobile menu button */}
+              <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                {open ? (
+                  <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                ) : (
+                  <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                )}
+              </Disclosure.Button>
+            </div>
+          </div>
+        </div>
+
+        <Disclosure.Panel className={" md:hidden"}>
+          <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+            {navigation.map((item) => (
+              <Disclosure.Button
+                key={item.name}
+                to={item.to}
+                className={({ isActive }) =>
+                  classNames(
+                    isActive
+                      ? "bg-gray-900 text-white"
+                      : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                    "block rounded-md px-3 py-2 text-base font-medium"
+                  )
+                }
+              >
+                {item.name}
+              </Disclosure.Button>
+            ))}
+          </div>
+          <div className="border-t border-gray-700 pb-3 pt-4">
+            <div className="flex items-center px-5">
+              <div className="flex-shrink-0">
+                <img
+                  className="h-10 w-10 rounded-full"
+                  src={user.imageUrl}
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="mt-3 space-y-1 px-2">
+              {signUser.map((item) => (
                 <Disclosure.Button
                   key={item.name}
+                  as="a"
                   to={item.to}
-                  className={({ isActive }) =>
-                    classNames(
-                      isActive
-                        ? "bg-gray-900 text-white"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "block rounded-md px-3 py-2 text-base font-medium"
-                    )
-                  }
+                  className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
                 >
                   {item.name}
                 </Disclosure.Button>
               ))}
             </div>
-            <div className="border-t border-gray-700 pb-3 pt-4">
-              <div className="flex items-center px-5">
-                <div className="flex-shrink-0">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src={user.imageUrl}
-                    alt=""
-                  />
-                </div>
-              </div>
-              <div className="mt-3 space-y-1 px-2">
-                {signUser.map((item) => (
-                  <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    to={item.to}
-                    className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-                  >
-                    {item.name}
-                  </Disclosure.Button>
-                ))}
-              </div>
-            </div>
-          </Disclosure.Panel>
-        </div>
+          </div>
+        </Disclosure.Panel>
       </Disclosure>
     </>
   );
