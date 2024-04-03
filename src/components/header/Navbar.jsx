78a147f72a69506a3e49a669c05e347690b62739
Robert Matundu <matundukabamba@gmail.com>
2024-04-03 16:27:54 +0200

    update

--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -42,10 +42,10 @@ const Navbar = () => {
         <Disclosure as="nav" className="bg-bleue">
           {({ open }) => (
             <>
-              <div className=" md:px-10 px-5 ">
+              <div className=" md:px-10 ">
                 <div className="flex items-center justify-between gap-2 md:px-10 px-9">
                   <div className="flex justify-start items-center gap-20">
-                    <div className=" flex-shrink-0">
+                    <div className=" flex-shrink-0 p-3">
                       <Link to={"/"}>
                         <img
                           src="../../public/vite.svg"
@@ -76,52 +76,53 @@ const Navbar = () => {
                       </div>
                     </div>
                   </div>
+                  <button className=" bg-dark p-6 text-dark font-bold text-2xl hidden md:block">
+                    {/* Profile dropdown */}
+                    <Menu as="div" className="relative ml-3">
+                      <div>
+                        <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm ring-2 ring-bleue ring-offset-2 ring-offset-gray-800 focus:outline-none focus:ring-2 focus:ring-bleue focus:ring-offset-2 focus:ring-offset-gray-800">
+                          <span className="absolute -inset-1.5" />
+                          <span className="sr-only">Open user menu</span>
+                          <img
+                            className="h-10 w-10 rounded-full"
+                            src={user.imageUrl}
+                            alt=""
+                          />
+                        </Menu.Button>
+                      </div>
+                      <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-100"
+                        enterFrom="transform opacity-0 scale-95"
+                        enterTo="transform opacity-100 scale-100"
+                        leave="transition ease-in duration-75"
+                        leaveFrom="transform opacity-100 scale-100"
+                        leaveTo="transform opacity-0 scale-95"
+                      >
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                          {signUser.map((item) => (
+                            <Menu.Item key={item.nom}>
+                              {({ active }) => (
+                                <Link
+                                  to={item.to}
+                                  className={classNames(
+                                    active ? "bg-grayClaire" : "",
+                                    "block px-4 py-2 text-sm text-dark hover:text-slate-700"
+                                  )}
+                                >
+                                  {item.nom}
+                                </Link>
+                              )}
+                            </Menu.Item>
+                          ))}
+                        </Menu.Items>
+                      </Transition>
+                    </Menu>
+                  </button>
 
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm ring-2 ring-bleue ring-offset-2 ring-offset-gray-800 focus:outline-none focus:ring-2 focus:ring-bleue focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span className="absolute -inset-1.5" />
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-10 w-10 rounded-full"
-                          src={user.imageUrl}
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        {signUser.map((item) => (
-                          <Menu.Item key={item.nom}>
-                            {({ active }) => (
-                              <Link
-                                to={item.to}
-                                className={classNames(
-                                  active ? "bg-grayClaire" : "",
-                                  "block px-4 py-2 text-sm text-dark hover:text-slate-700"
-                                )}
-                              >
-                                {item.nom}
-                              </Link>
-                            )}
-                          </Menu.Item>
-                        ))}
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-
-                  <div className="-mr-2 flex md:hidden">
+                  <div className="-mr-2 flex md:hidden p-3">
                     {/* Mobile menu button */}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md bg-purple p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="absolute -inset-0.5" />
                       <span className="sr-only">Open main menu</span>
                       {open ? (
