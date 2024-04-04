79151b6f3ae3d2947be024c56546f1f16b8b8293
Robert Matundu <matundukabamba@gmail.com>
2024-04-04 14:42:08 +0200

    update

--- src/components/component_ecole/Panel.jsx
+++ src/components/component_ecole/Panel.jsx
@@ -10,9 +10,9 @@ const Panel = () => {
 
   return (
     <>
-      <section className="mx-20 my-10">
+      <section className="mx-5 md:mx-20 my-10 md:my-8">
         <div className="grid md:grid-cols-3 py-4 rounded-md gap-5">
-          <div className="flex items-center gap-5 w-full justify-between md:px-10 bg-gradient-to-r from-pink to-middark py-5 rounded-md shadow-md shadow-middark">
+          <div className="flex items-center gap-5 w-full justify-between md:px-10 px-10 bg-gradient-to-r from-pink to-middark py-5 rounded-md shadow-md shadow-middark">
             <div>
               <div className="text-xl font-semibold text-white">Classe A</div>
               <div className=" text-sm text-white">Première Classe</div>
